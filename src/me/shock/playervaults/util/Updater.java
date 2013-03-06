2fdebb47c5b5ec85acaa78fb7101c96da1a8668e
gomeow <davismariotti@gmail.com>
2013-03-05 21:49:21 -0800

    Remove misleading comments

--- src/me/shock/playervaults/util/Updater.java
+++ src/me/shock/playervaults/util/Updater.java
@@ -1,11 +1,5 @@
 package me.shock.playervaults.util;
 
-/*
- * Updater for Bukkit.
- *
- * This class provides the means to safetly and easily update a plugin, or check to see if it is updated using dev.bukkit.org
- */
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -13,21 +7,6 @@ import java.net.UnknownHostException;
 
 import me.shock.playervaults.Main;
 
-/**
- * Check dev.bukkit.org to find updates for a given plugin, and download the updates if needed.
- * <p>
- * <b>VERY, VERY IMPORTANT</b>: Because there are no standards for adding auto-update toggles in your plugin's config, this system provides NO CHECK WITH YOUR CONFIG to make sure the user has allowed auto-updating.
- * <br>
- * It is a <b>BUKKIT POLICY</b> that you include a boolean value in your config that prevents the auto-updater from running <b>AT ALL</b>.
- * <br>
- * If you fail to include this option in your config, your plugin will be <b>REJECTED</b> when you attempt to submit it to dev.bukkit.org.
- * <p>
- * An example of a good configuration option would be something similar to 'auto-update: true' - if this value is set to false you may NOT run the auto-updater.
- * <br>
- * If you are unsure about these rules, please read the plugin submission guidelines: http://goo.gl/8iU5l
- *
- * @author H31IX
- */
 
 public class Updater extends Main {
 	
