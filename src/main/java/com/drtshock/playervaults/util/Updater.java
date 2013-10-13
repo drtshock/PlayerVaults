89a7e47577886d0350ca9a9f9de4b3ba6999ebc9
drtshock <drtshock13@gmail.com>
2013-10-12 19:41:38 -0500

    Let's be GPL. Change version to snapshot

--- src/main/java/com/drtshock/playervaults/util/Updater.java
+++ src/main/java/com/drtshock/playervaults/util/Updater.java
@@ -1,9 +1,19 @@
-/*
- * Updater for Bukkit.
+/* 
+ * Copyright (C) 2013 drtshock
  *
- * This class provides the means to safely and easily update a plugin, or check to see if it is updated using dev.bukkit.org
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package com.drtshock.playervaults.util;
 
 import java.io.*;
