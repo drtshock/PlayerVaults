259b7b907e94cc2731f4e9292827e9a2a45a606d
gomeow <davismariotti@gmail.com>
2013-12-28 23:31:13 -0800

    Fix stupid bad

--- src/main/java/com/drtshock/playervaults/util/Metrics.java
+++ src/main/java/com/drtshock/playervaults/util/Metrics.java
@@ -25,7 +25,7 @@
  * authors and contributors and should not be interpreted as representing official policies,
  * either expressed or implied, of anybody else.
  */
-package io.snw.tutorial.util;
+package com.drtshock.playervaults.util;
 
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.InvalidConfigurationException;
