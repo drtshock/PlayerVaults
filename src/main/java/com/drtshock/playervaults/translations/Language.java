a7371229de7ea81face234001f930db55eca2ff7
Fix3disTaken <serhanduzce@gmail.com>
2018-03-31 00:40:39 +0300

    Update Language.java

--- src/main/java/com/drtshock/playervaults/translations/Language.java
+++ src/main/java/com/drtshock/playervaults/translations/Language.java
@@ -3,6 +3,7 @@ package com.drtshock.playervaults.translations;
 public enum Language {
     ENGLISH("english"),
     BULGARIAN("bulgarian"),
+    TURKISH("turkish"),
     DUTCH("dutch");
 
     private String friendlyName;
