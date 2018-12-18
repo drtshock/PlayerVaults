f463dedee6fff520119a00aa7be24a4b254284e1
Trent Hensler <drtshock13@gmail.com>
2018-12-17 20:08:56 -0800

    Update license headers in all files.

--- src/main/java/com/drtshock/playervaults/listeners/Listeners.java
+++ src/main/java/com/drtshock/playervaults/listeners/Listeners.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2013 drtshock
+ * PlayerVaultsX
+ * Copyright (C) 2013 Trent Hensler
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,6 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package com.drtshock.playervaults.listeners;
 
 import com.drtshock.playervaults.PlayerVaults;
