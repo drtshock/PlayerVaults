077745733b53e7832ab29f01379f71a6b5ccb6ca
Trent Hensler <drtshock13@gmail.com>
2018-12-29 15:45:06 -0800

    Send messages the right way. Fixes #378

--- src/main/java/com/drtshock/playervaults/commands/DeleteCommand.java
+++ src/main/java/com/drtshock/playervaults/commands/DeleteCommand.java
@@ -18,7 +18,9 @@
 
 package com.drtshock.playervaults.commands;
 
+import com.drtshock.playervaults.PlayerVaults;
 import com.drtshock.playervaults.translations.Lang;
+import com.drtshock.playervaults.vaultmanagement.VaultManager;
 import com.drtshock.playervaults.vaultmanagement.VaultOperations;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -53,9 +55,14 @@ public class DeleteCommand implements CommandExecutor {
 
                 // TODO: fix the stupid message inconsistencies where sometimes this class sends, sometimes vaultops does.
                 if (args[1].equalsIgnoreCase("all")) {
-                    VaultOperations.deleteOtherAllVaults(sender, target);
-                    sender.sendMessage(Lang.TITLE.toString() + Lang.DELETE_OTHER_VAULT_ALL.toString().replaceAll("%p", target));
-                    break;
+                    if (sender.hasPermission("playervaults.delete.all")) {
+                        VaultManager.getInstance().deleteAllVaults(target);
+                        sender.sendMessage(Lang.TITLE.toString() + Lang.DELETE_OTHER_VAULT_ALL.toString().replaceAll("%p", target));
+                        PlayerVaults.getInstance().getLogger().info(String.format("%s deleted ALL vaults belonging to %s", sender.getName(), target));
+                    } else {
+                        sender.sendMessage(Lang.TITLE.toString() + Lang.NO_PERMS);
+                    }
+
                 }
                 VaultOperations.deleteOtherVault(sender, target, args[1]);
                 break;
