5ef72c893cbe61ae717caf05a9cd31673eb146a3
mbax <matt@phozop.net>
2024-01-06 20:26:35 -0500

    Support Spigot 1.20.4 and better detect Spigot issues

--- src/main/java/com/drtshock/playervaults/PlayerVaults.java
+++ src/main/java/com/drtshock/playervaults/PlayerVaults.java
@@ -50,10 +50,12 @@ import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.kitteh.cardboardbox.CardboardBox;
 import sun.misc.Unsafe;
 
 import java.io.BufferedReader;
@@ -133,6 +135,17 @@ public class PlayerVaults extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        if (!CardboardBox.isReady()) {
+            Exception ex = null;
+            try {
+                CardboardBox.serializeItem(new ItemStack(Material.STONE));
+            } catch (Exception e) {
+                ex = e;
+            }
+            this.getLogger().log(Level.SEVERE, "Could not initialize!", ex);
+            this.getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
         instance = this;
         long start = System.currentTimeMillis();
         long time = System.currentTimeMillis();
